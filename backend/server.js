0e61a86879f606a7986ed4a47b1dbf316e2046ff
Gianna <giannasfrisi@gmail.com>
2023-12-02 16:05:06 -0500

    created the sql database, added the connection to server

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// working with the data base
+app.get('/users', (req, res) => {
+  db.query('SELECT * FROM new_user_repositories', (err, results) => {
+      if (err) throw err;
+      res.json(results);
+  });
 });
 
+// starts server
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
---
+++ database/database.js
@@ -0,0 +1,19 @@
+// for the connection to the database
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost', 
+    user: 'giannasfrisi@gmail.com', 
+    password: 'JosieandAriel123!', 
+    database: 'codecommentordatabase'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting: ' + err.stack);
+        return;
+    }
+    console.log('Connected as id ' + connection.threadId);
+});
+
+module.exports = connection;
