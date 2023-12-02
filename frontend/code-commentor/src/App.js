dba494a6bad391b32f4f13469f3890a421156e1e
FlareCoding <albert.slepak@gmail.com>
2023-12-02 14:35:25 -0500

    added state logs to check if oauth or local storage is being used for token access

--- frontend/code-commentor/src/App.js
+++ frontend/code-commentor/src/App.js
@@ -6,15 +6,25 @@ function App() {
   const [userData, setUserData] = useState(null);
 
   useEffect(() => {
-    // Parse the URL to check for OAuth callback data
-    const hash = window.location.hash;
-    if (hash) {
-      const params = new URLSearchParams(hash.slice(1)); // Remove the '#' part
-      const accessToken = params.get('access_token');
+    const accessToken = localStorage.getItem('accessToken');
+    
+    if (accessToken) {
+      console.log('Using access token from local storage.');
+      fetchUserProfile(accessToken);
+    } else {
+      console.log('No access token in local storage. User needs to log in.');
 
-      if (accessToken) {
-        localStorage.setItem('accessToken', accessToken);
-        fetchUserProfile(accessToken);
+      // Parse the URL to check for OAuth callback data
+      const hash = window.location.hash;
+      if (hash) {
+        const params = new URLSearchParams(hash.slice(1)); // Remove the '#' part
+        const newAccessToken = params.get('access_token');
+
+        if (newAccessToken) {
+          console.log('New access token received from OAuth.');
+          localStorage.setItem('accessToken', newAccessToken);
+          fetchUserProfile(newAccessToken);
+        }
       }
     }
   }, []);
@@ -23,14 +33,16 @@ function App() {
     try {
       const data = await getUserProfile(accessToken);
       setUserData(data);
+      console.log('User data fetched successfully.');
     } catch (error) {
-      console.error('Error:', error);
+      console.error('Error fetching user data:', error);
     }
   };
 
   const logout = () => {
     localStorage.removeItem('accessToken');
     setUserData(null); // Update the state to reflect that the user is logged out
+    console.log('User logged out. Access token removed from local storage.');
   };
 
   return (
--- frontend/code-commentor/src/Login.js
+++ frontend/code-commentor/src/Login.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 function Login() {
-const handleLogin = () => {
-    const CLIENT_ID = '349917344619-3qjnujqd3k4sc086v4gq49kb956rtr6u.apps.googleusercontent.com';
-    const REDIRECT_URI = encodeURIComponent('http://localhost:3000/callback');
-    const SCOPE = encodeURIComponent('email profile');
+    const handleLogin = () => {
+        const CLIENT_ID = '349917344619-3qjnujqd3k4sc086v4gq49kb956rtr6u.apps.googleusercontent.com';
+        const REDIRECT_URI = encodeURIComponent('http://localhost:3000/callback');
+        const SCOPE = encodeURIComponent('email profile');
 
-    const oauthUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=token&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}`;
+        const oauthUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=token&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}`;
 
-    window.location.href = oauthUrl;
-};
+        window.location.href = oauthUrl;
+    };
 
     const buttonStyle = {
         padding: '10px 20px',
