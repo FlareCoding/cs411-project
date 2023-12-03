808b95e22c4240aa5d35c979c41054638a17839d
FlareCoding <albert.slepak@gmail.com>
2023-12-02 19:24:30 -0500

    refactored api calling prototype code

--- backend/server.js
+++ backend/server.js
@@ -1,52 +1,14 @@
 const db = require('../database/database.js');
-
 const express = require('express');
-// const OpenAI = require('openai');
 const thirdPartyApi = require('./third-party-api');
 const app = express();
-// require('dotenv').config();
 
-// const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
-
-// TEST_PROMPT = `
-// Please document the following functions in a doxygen style comment above the function declaration only
-// uint64_t _resetXhciController(int status) {
-//   // some code
-// }
-// `;
-
-// let chatApiCall = async () => {
-//   try {
-//     const chatCompletion = await openai.chat.completions.create({
-//       model: "gpt-3.5-turbo",
-//       messages: [{"role": "user", "content": TEST_PROMPT}],
-//     });
-//     console.log(chatCompletion.choices[0].message);
-//   } catch (error) {
-//     if (error instanceof OpenAI.APIError) {
-//       console.error(error.status);  // e.g. 401
-//       console.error(error.message); // e.g. The authentication token you passed was invalid...
-//       console.error(error.code);  // e.g. 'invalid_api_key'
-//       console.error(error.type);  // e.g. 'invalid_request_error'
-//     } else {
-//       // Non-API error
-//       console.log(error);
-//     }
-//   }
-// }
-
-// chatApiCall();
-
-// // Example usage
-// getRepoContents('https://github.com/FlareCoding/tcp_friend').then(files => {
-//   console.log(files);
-// });
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
-
-// working with the data base
+TEST_PROMPT = `
+Please document the following functions in a doxygen style comment above the function declaration only
+uint64_t _resetXhciController(int status) {
+  // some code
+}
+`;
 
 app.get('/users', (req, res) => {
   db.query('SELECT * FROM new_user_repositories', (err, results) => {
--- backend/third-party-api.js
+++ backend/third-party-api.js
@@ -1,4 +1,8 @@
-const axios = require('axios')
+const axios = require('axios');
+const OpenAI = require('openai');
+require('dotenv').config();
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 async function fetchGithubRepoContents(repoUrl, path = '') {
     try {
@@ -27,6 +31,27 @@ async function fetchGithubRepoContents(repoUrl, path = '') {
     }
 }
 
+async function requestChatGptResponse(prompt) {
+  try {
+    const chatCompletion = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [{"role": "user", "content": prompt}],
+    });
+    console.log(chatCompletion.choices[0].message);
+  } catch (error) {
+    if (error instanceof OpenAI.APIError) {
+      console.error(error.status);  // e.g. 401
+      console.error(error.message); // e.g. The authentication token you passed was invalid...
+      console.error(error.code);  // e.g. 'invalid_api_key'
+      console.error(error.type);  // e.g. 'invalid_request_error'
+    } else {
+      // Non-API error
+      console.log(error);
+    }
+  }
+}
+
 module.exports = {
-    fetchGithubRepoContents
+    fetchGithubRepoContents,
+    requestChatGptResponse
 }
